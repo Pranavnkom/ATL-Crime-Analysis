ff3d79a5991f8def1028e9fb215c47a0689a7335
Pranav <pranav2000jan1@gmail.com>
2018-10-11 13:47:30 -0400

    3rd commit

--- main.py
+++
@@ -1,9 +0,0 @@
-import functools
-
-from flask import (
-    Blueprint, flash, g, redirect, home_template, request, session, url_for
-)
-
-from flaskr.db import get_db
-
-bp = Blueprint('main', __name__, url_prefix='/main')
